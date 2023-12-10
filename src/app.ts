9e6e09afde14c05e75e81032b5caa6ccb1f66132
BitLopezDev <contacto@bitopez.bio>
2023-12-10 16:23:16 -0300

    api to openai

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
 import express, { Request, Response } from "express";
 import path from "path";
 import { SendFileOptions } from "express-serve-static-core";
+require("dotenv").config();
 const app = express();
+const OpenAI = require("openai");
 const port = 3000;
+const ai = new OpenAI({
+  apiKey: process.env.APIKEY,
+});
 
 app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/gpt", async (req: Request, res: Response) => {
+  const response = await ai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: "Tell me a fun fact",
+      },
+    ],
+    max_tokens: 100,
+  });
+  const fact = JSON.stringify(response.choices[0].message.content);
+  res.json({ fact });
+});
 
 app.get("/", (req: Request, res: Response) => {
   // Send the HTML file
@@ -18,5 +38,5 @@ app.get("/data", (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  // console.log(`Server listening at http://localhost:${port}`);
 });
